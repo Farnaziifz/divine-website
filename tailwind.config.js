760358142759fd24663b090792c84e3a5728a4e4
farnaz <ffarzipour92@gmail.com>
2024-06-20 15:42:48 +0330

    init project

---
+++ tailwind.config.js
@@ -0,0 +1,47 @@
+module.exports = {
+  content: [
+    './components/**/*.{js,vue,ts}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './plugins/**/*.{js,ts}',
+    './app.vue',
+    './error.vue',
+  ],
+  theme: {
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
+    },
+    colors: {
+      primary: '#F1AF09',
+      'sec-gray': '#CCCCCC',
+      white: '#fff',
+      transparent: 'transparent',
+      'light-success': '#C9F9D1',
+      'text-gray': '#828181',
+      danger: '#9a2626',
+    },
+    container: {
+      center: true,
+    },
+    extend: {
+      order: {
+        1: '1',
+        2: '2',
+        3: '3',
+        4: '4',
+        5: '5',
+        6: '6',
+      },
+      height: {
+        img: '400px',
+      },
+    },
+  },
+  plugins: [
+    // require('@tailwindcss/forms'),
+  ],
+}
